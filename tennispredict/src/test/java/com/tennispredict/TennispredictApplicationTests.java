1b3288c00566522db45cc4e3707707f7ad8b13cd
Jan Olszewski <warjohnolsz@gmail.com>
2022-03-18 19:20:56 +0100

    Tennis initialize

---
+++ tennispredict/src/test/java/com/tennispredict/TennispredictApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tennispredict;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennispredictApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
