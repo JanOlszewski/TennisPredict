64cb9e3e9c6c2c04668e6005ea4611eb807147b8
Jan Olszewski <warjohnolsz@gmail.com>
2022-03-19 16:23:24 +0100

    Primary working version

---
+++ tennispredict/backend/web/src/main/java/com/tennispredict/TennispredictApplication.java
@@ -0,0 +1,13 @@
+package com.tennispredict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennispredictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennispredictApplication.class, args);
+    }
+
+}
---
+++ tennispredict/backend/web/src/main/java/com/tennispredict/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.tennispredict.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/app")
+public class IndexController {
+
+    @GetMapping(value = "")
+    public String index() {
+        return "forward:app/index.html";
+    }
+
+    @GetMapping(value = "/")
+    public String indexSlashed() {
+        return "forward:index.html";
+    }
+
+}
